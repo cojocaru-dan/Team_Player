30ef662cbe43592cbcc270b241d0f405c0621784
Laura <lauraandradac15@gmail.com>
2023-02-20 14:27:30 +0200

    Team Members

--- data.js
+++ data.js
@@ -1,13 +1,53 @@
+
+// Our Team
+
 const ourTeam = {
 	name: "LearnZilla",
-	members: ["Buzila Ionut", "Ciobanu Laura", "Cojocaru Dan"]
-};
+	members: [
+		{
+			name: "Buzila Ionut",
+			birthday: {
+				year: 1999,
+				month: 10,
+				day: 19
+			},
+			location: {
+				country: "Romania",
+				city: "Nasaud"
+			},
+			codingLevel: 66,
+		},
+		{
+			name: "Ciobanu Laura",
+			birthday: {
+				year: 1996,
+				month: 10,
+				day: 15,
+			},
+			location: {
+				country: "Romania",
+				city: "Iasi",
+			},
+			codingLevel: 55,
+		},
+		{
+			name:"Cojocaru Dan",
+			birthday: {
+				year: 1990,
+				month: 6, 
+				day: 20,
+			},
+			location: {
+				country: "Romania",
+				city: "Bucuresti",
+			},
+			codingLevel: 70,
+	}
+]
+	}
 
 
-
-
-
-
+// Team Members
 
 
 
